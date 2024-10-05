bafc324a0b142955fdbdd58cdc41ad66d1230960
komalpalande <143799718+komalpalande@users.noreply.github.com>
2024-10-05 01:40:09 -0700

    Add files via upload

---
+++ Lectureseven4.java
@@ -0,0 +1,11 @@
+package Assignment;
+
+public class Lectureseven4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+myclass();
+System.out.println("21");
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class Main {
+	// Driver class
+		      // Main function
+		    public static void main(String[] args)
+		    {
+		        Two g = new Two();
+		        g.print_geek();
+		        g.print_for();
+		        g.print_geek();
+		  
+
+	}
+
+
+}
