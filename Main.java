dec35fe05aec788e6c97d65eb76c507686a10991
komalpalande <143799718+komalpalande@users.noreply.github.com>
2024-10-07 11:48:35 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,27 @@
-package Assignment;
+package chap1;
 
-public class Main {
-	// Driver class
-		      // Main function
-		    public static void main(String[] args)
-		    {
-		        Two g = new Two();
-		        g.print_geek();
-		        g.print_for();
-		        g.print_geek();
-		  
-
-	}
-
-
+class Main {
+    public static void main(String[] args) {
+        // Create SilverCardCustomer object
+        SilverCardCustomer silverCustomer = new SilverCardCustomer("John Doe", "1234567890123456");
+        silverCustomer.viewCreditAmount();
+        silverCustomer.useCard(10000);
+        silverCustomer.payCredit(5000);
+        silverCustomer.increaseLimit(3000); // Should display that Silver card customers can't increase limit
+        silverCustomer.viewCreditAmount();
+        
+        System.out.println("-----");
+        
+        // Create GoldCardCustomer object
+        GoldCardCustomer goldCustomer = new GoldCardCustomer("Jane Doe", "9876543210987654");
+        goldCustomer.viewCreditAmount();
+        goldCustomer.useCard(50000);
+        goldCustomer.payCredit(20000);
+        goldCustomer.increaseLimit(4000); // Should increase limit by 4000
+        goldCustomer.increaseLimit(6000); // Should display limit increase failure due to limit
+        goldCustomer.increaseLimit(3000); // Should increase limit by 3000
+        goldCustomer.increaseLimit(5000); // Should increase limit by 5000
+        goldCustomer.increaseLimit(1000); // Should fail as the increase limit count is 3
+        goldCustomer.viewCreditAmount();
+    }
 }
